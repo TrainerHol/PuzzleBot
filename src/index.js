2be32bde83c3f159361dec4ba53745faa552db3a
TrainerHol <79064972+TrainerHol@users.noreply.github.com>
2024-05-28 22:37:12 -0700

    Update status cycling

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const {
   MessageManager,
   Embed,
   Collection,
+  ActivityType,
 } = require("discord.js");
 const fs = require("fs");
 const client = new Client({
@@ -31,6 +32,37 @@ const eventFiles = fs
   .filter((file) => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
+// Array of statuses to cycle through
+const statuses = [
+  { name: "void furniture destroy the servers.", type: ActivityType.Watching },
+  { name: "ffxiv.ju.mp", type: ActivityType.Playing },
+  { name: "people fall.", type: ActivityType.Watching },
+  { name: "message book signing.", type: ActivityType.Competing },
+  { name: "4★ btw!", type: ActivityType.Playing },
+  {
+    name: "trainerhol.github.io/Strange-Housing/",
+    type: ActivityType.Playing,
+  },
+  { name: "ankles breaking.", type: ActivityType.Listening },
+  { name: "with blue vases.", type: ActivityType.Playing },
+  { name: "Hell on High.", type: ActivityType.Competing },
+  { name: "your /badgelist.", type: ActivityType.Watching },
+  { name: "people get nucked.", type: ActivityType.Watching },
+  { name: "Heavensward Diadem.", type: ActivityType.Competing },
+  { name: "FINAL FANTASY XIV", type: ActivityType.Playing },
+  { name: "move to Front Door.", type: ActivityType.Playing },
+  // What else?
+];
+
+let currentStatusIndex = 0;
+
+function cycleStatus() {
+  const status = statuses[currentStatusIndex];
+  client.user.setPresence({ activities: [status], status: "online" });
+
+  currentStatusIndex = (currentStatusIndex + 1) % statuses.length;
+}
+
 async function downloadPuzzles() {
   try {
     await sequelize.authenticate();
@@ -47,10 +79,18 @@ async function downloadPuzzles() {
   }
 }
 
+client.once("ready", () => {
+  // Set initial status
+  cycleStatus();
+
+  // Cycle through statuses every 15 seconds
+  setInterval(cycleStatus, 15000);
+});
+
 (async () => {
   try {
     // Download puzzles at startup
-    await downloadPuzzles();
+    //await downloadPuzzles();
 
     // Schedule the puzzle download every 12 hours
     cron.schedule("0 */12 * * *", downloadPuzzles);
